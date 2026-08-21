384d12e7fb94c9223491af89d7da7e8dc4dd54f8
pranitik100-bit <pranitik100@gmail.com>
2026-08-22 01:28:13 +0530

    Set up Flutter app with Firebase initialization
    Initialize Firebase and set up main app structure.

--- main.dart
+++ main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'student/screens/student_home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: StudentHome(),
+    );
+  }
+}
+
+
 
